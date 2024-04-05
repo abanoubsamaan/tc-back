5066f64befc86544291c960ad5154f9d0a89a67c
abanoub samaan <admin@ch4host.com>
2024-04-05 17:55:38 -0400

    database migrations

---
+++ database/migrations/2024_04_05_212806_create_purchase_order_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('purchase_order_id');
+            $table->string('description');
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_price');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamps();
+
+            $table->foreign('purchase_order_id')
+                ->references('id')
+                ->on('purchase_orders')
+                ->onDelete('cascade');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_order_items');
+    }
+};
