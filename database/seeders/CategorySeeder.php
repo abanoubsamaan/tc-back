d7fbeb76f7184992311349f7a0497589bd065008
abanoub samaan <admin@ch4host.com>
2024-04-05 18:23:44 -0400

    Create categories seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'];
+
+        foreach($categories as $category){
+            Category::create(['name' => $category]);
+        }
+    }
+}
