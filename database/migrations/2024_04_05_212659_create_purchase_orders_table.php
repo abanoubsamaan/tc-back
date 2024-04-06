06a8d1aaa6921601cb9565c61fbd9b3a451ea915
abanoub samaan <admin@ch4host.com>
2024-04-06 10:40:28 -0400

    Fix migration issue in the purchase order table

--- database/migrations/2024_04_05_212659_create_purchase_orders_table.php
+++ database/migrations/2024_04_05_212659_create_purchase_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('po_number');
             $table->string('buyer_name');
-            $table->decimal('total',10);
+            $table->decimal('total',10)->default(0);
             $table->timestamps();
         });
     }
