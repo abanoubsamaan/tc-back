b886dbf03dda24aaec66815a4d4dba98256c95c7
abanoub samaan <admin@ch4host.com>
2024-04-05 21:57:45 -0400

    Create models fillables and relationships

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseOrder extends Model
 {
     use HasFactory;
+    protected $fillable = ['po_number', 'buyer_name', 'total'];
+
+    public function items(){
+        return $this->hasMany(PurchaseOrderItem::class, 'purchase_order_id');
+    }
 }
--- app/Models/PurchaseOrderItem.php
+++ app/Models/PurchaseOrderItem.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class PurchaseOrderItem extends Model
 {
     use HasFactory;
+    protected $fillable = ['purchase_order_id','po_number', 'description', 'quantity', 'unit_price', 'category_id'];
 }
