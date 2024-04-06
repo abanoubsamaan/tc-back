8a24d8ebd06864c0350e4049567d834eb7ad27ac
abanoub samaan <admin@ch4host.com>
2024-04-06 10:40:45 -0400

    create category relationship

--- app/Models/PurchaseOrderItem.php
+++ app/Models/PurchaseOrderItem.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PurchaseOrderItem extends Model
 {
     use HasFactory;
-    protected $fillable = ['purchase_order_id','po_number', 'description', 'quantity', 'unit_price', 'category_id'];
+    protected $fillable = ['purchase_order_id', 'description', 'quantity', 'unit_price', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
